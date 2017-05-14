2168e0086f87e6aa0410f4f21ecf10acbde17da1
Alex <alpe0504@gmail.com>
2017-05-14 13:29:15 +0200

    weaken ability

---
+++ TyrantProject/Projects/Game/Game/WeakenAbility.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "WeakenAbility.h"
+
+
+WeakenAbility::WeakenAbility(const string & aSuffix, char aNumber, eCardFaction aSpecificFaction): AbilityBase(aSuffix,aNumber,aSpecificFaction)
+{
+	iconTexturePath = "Data/Textures/Icons/Skills/weakenIcon.png";
+}
+
+WeakenAbility::~WeakenAbility()
+{
+}
+
+void WeakenAbility::OnPreCombat(Card * aCard)
+{
+	if (aCard->GetCooldown() < 1)
+	{
+		if (mySuffix.Lenght() == 0)
+		{
+			Card* target = FindTargetOffCoolDown(aCard->GetOwner()->GetOpponent()->GetAssaultCards());
+
+			if (target != nullptr)
+			{
+				AbilityStack::AddAbility(this, aCard, target);
+			}
+		}
+		else if (mySuffix == "all")
+		{
+			CU::GrowingArray<Card*> targets = FindAllTargetsOffCoolDown(aCard->GetOwner()->GetOpponent()->GetAssaultCards());
+
+			if (targets.Size() > 0)
+			{
+				AbilityStack::AddAbility(this, aCard, targets);
+			}
+		}
+	}
+}
+
+void WeakenAbility::DoAction(Card * aCaster, CU::GrowingArray<Card*>& someTargets)
+{
+	aCaster;
+	for (int i = 0; i < someTargets.Size(); ++i)
+	{
+		someTargets[i]->Weaken(myNumber);
+	}
+}
---
+++ TyrantProject/Projects/Game/Game/WeakenAbility.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "AbilityBase.h"
+
+class WeakenAbility: public AbilityBase
+{
+public:
+	WeakenAbility(const string& aSuffix, char aNumber, eCardFaction aSpecificFaction);
+	~WeakenAbility();
+
+	void OnPreCombat(Card* aCard) override;
+
+	void DoAction(Card* aCaster, CU::GrowingArray<Card*>& someTargets) override;
+};
+
