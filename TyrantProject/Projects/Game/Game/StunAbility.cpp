38191eaecc57ec3247c93b521b5855cc6ee05b0a
Alex <alpe0504@gmail.com>
2017-05-21 17:22:28 +0200

    buga och bock för världens fulatse stun animation XD

--- TyrantProject/Projects/Game/Game/StunAbility.cpp
+++ TyrantProject/Projects/Game/Game/StunAbility.cpp
@@ -2,6 +2,17 @@
 #include "StunAbility.h"
 
 
+Vector2<float> stunAnimationSize(1.5f, 2.f);
+AnimationData stunAnimation = AnimationData
+(
+	"Data/Textures/Animations/stunAnimation.png",
+	8,
+	2,
+	20.f,
+	false
+	);
+
+
 StunAbility::StunAbility()
 {
 	myAbilityType = eAbilityTypes::eStun;
@@ -18,4 +29,5 @@ void StunAbility::OnAttacked(Card * aUser, char & someDamage, Card * anAttacker)
 	aUser;
 	someDamage;
 	anAttacker->Stun();
+	AnimationManager::AddAnimation(stunAnimation, anAttacker->GetPosition(), stunAnimationSize);
 }
